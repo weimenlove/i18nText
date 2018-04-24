c1b47111b4209589fa9d8442c0c855620a69c8cc
Matthew Wei <weimenlove@gmail.com>
2018-04-24 20:22:35 +0800

    add groud image and merge tow image

--- main.cpp
+++ main.cpp
@@ -22,27 +22,25 @@ int main(int argc, char **argv) {
 #if 1
         memset(img_buf, 0, sizeof(img_buf));
         fread(img_buf, 200, 1024, fp);
-        Mat img2 = Mat(1080, 1920, CV_8UC3, img_buf);
-        Mat img = imdecode(img2, CV_LOAD_IMAGE_COLOR);
+        Mat img_jpeg = Mat(1080, 1920, CV_8UC3, img_buf);
+        Mat img = imdecode(img_jpeg, CV_LOAD_IMAGE_COLOR);
+        Mat img_groud = Mat(150, 1920, CV_8UC3, Scalar(200, 215, 230));
 #else
         Mat img = imread(argv[1], -1);   // Read the file
 #endif
-
-        cout << "img.flags: " << img.flags << endl;
-        cout << "img.dims: " << img.dims << endl;
-        cout << "img.rows: " << img.rows << endl;
-        cout << "img.cols: " << img.cols << endl;
-        cout << "img.depth(): " << img.depth() << endl;
         i18nText i18n;
         if (i18n.setFont("simsun.ttc")) {
           cout << "Load fonts successfully." << endl;
           const wchar_t *msg = L"2018-04-20 16:25:25 抓拍图片";
           cout << "Put ";
-          int num = i18n.putText(img, msg, Point(25, 50), CV_RGB(255, 0, 0));
+          int num = i18n.putText(img_groud, msg, Point(25, 50), CV_RGB(0, 0, 0));
           cout << num << " chars." << endl;
           compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
-          compression_params.push_back(37);
-          imwrite("test.jpg", img, compression_params);
+          compression_params.push_back(80);
+          Mat new_img;
+          new_img.push_back(img);
+          new_img.push_back(img_groud);
+          imwrite("test.jpg", new_img, compression_params);
         }
         fclose(fp);
     } catch (Exception e) {
