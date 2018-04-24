97d96d7df872d3a9bf5a85913a4e6ea9c78faac3
Matthew Wei <weimenlove@gmail.com>
2018-04-24 09:23:44 +0800

    change jpeg format data to yuv data

--- main.cpp
+++ main.cpp
@@ -8,9 +8,8 @@ using namespace std;
 
 int main(int argc, char **argv) {
     FILE *fp;
-    char img_buf[200*1024];
+    char img_buf[200*1024+1];
     vector<int> compression_params;
-    vector<char *> v;
 
     try {
         fp = fopen("0B17E0002018032717164200000001_SB.jpg", "r");
@@ -20,29 +19,30 @@ int main(int argc, char **argv) {
             return -1;
         }
 
+#if 1
         memset(img_buf, 0, sizeof(img_buf));
-        fread(img_buf, 199, 1024, fp);
-        v.push_back(img_buf);
+        fread(img_buf, 200, 1024, fp);
+        Mat img2 = Mat(1080, 1920, CV_8UC3, img_buf);
+        Mat img = imdecode(img2, CV_LOAD_IMAGE_COLOR);
+#else
+        Mat img = imread(argv[1], -1);   // Read the file
+#endif
 
-        Mat img2 = imdecode(v, -1);
-
-        cout << "img2.flags: " << img2.flags << endl;
-        cout << "img2.dims: " << img2.dims << endl;
-        cout << "img2.rows: " << img2.rows << endl;
-        cout << "img2.cols: " << img2.cols << endl;
-        cout << "img2.depth(): " << img2.depth() << endl;
-
-        //Mat image = imread(argv[1], -1);   // Read the file
+        cout << "img.flags: " << img.flags << endl;
+        cout << "img.dims: " << img.dims << endl;
+        cout << "img.rows: " << img.rows << endl;
+        cout << "img.cols: " << img.cols << endl;
+        cout << "img.depth(): " << img.depth() << endl;
         i18nText i18n;
         if (i18n.setFont("simsun.ttc")) {
           cout << "Load fonts successfully." << endl;
           const wchar_t *msg = L"2018-04-20 16:25:25 抓拍图片";
           cout << "Put ";
-          int num = i18n.putText(img2, msg, Point(25, 50), CV_RGB(255, 0, 0));
+          int num = i18n.putText(img, msg, Point(25, 50), CV_RGB(255, 0, 0));
           cout << num << " chars." << endl;
           compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
           compression_params.push_back(37);
-          imwrite("test.jpg", img2, compression_params);
+          imwrite("test.jpg", img, compression_params);
         }
         fclose(fp);
     } catch (Exception e) {
