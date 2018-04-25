61a297fe253d3d9f1917e56a86c00cdd6ab143da
Matthew Wei <weimenlove@gmail.com>
2018-04-25 16:58:54 +0800

    add two line text

--- main.cpp
+++ main.cpp
@@ -28,20 +28,38 @@ int main(int argc, char **argv) {
 #else
         Mat img = imread(argv[1], -1);   // Read the file
 #endif
-        i18nText i18n;
-        if (i18n.setFont("simsun.ttc")) {
-          cout << "Load fonts successfully." << endl;
-          const wchar_t *msg = L"2018-04-20 16:25:25 抓拍图片";
-          cout << "Put ";
-          int num = i18n.putText(img_groud, msg, Point(25, 50), CV_RGB(0, 0, 0));
-          cout << num << " chars." << endl;
-          compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
-          compression_params.push_back(80);
-          Mat new_img;
-          new_img.push_back(img);
-          new_img.push_back(img_groud);
-          imwrite("test.jpg", new_img, compression_params);
+        i18nText i18n1;
+        if (i18n1.setFont("simkai.ttf")) {
+            cout << "Load fonts successfully." << endl;
+            const wchar_t *msg1 = L"云停车一体机出口(识别抓拍)";
+            const wchar_t *msg2 = L"2018-04-25 17:18:25";
+            cout << "Put 1";
+            int num = i18n1.putText(img_groud, msg1, Point(25, 50), CV_RGB(0, 0, 0));
+            cout << num << " chars." << endl;
+            cout << "Put 2";
+            num = i18n1.putText(img_groud, msg2, Point(25, 120), CV_RGB(0, 0, 0));
+            cout << num << " chars." << endl;
         }
+        i18nText i18n2;
+        if (i18n2.setFont("simkai.ttf")) {
+            cout << "Load fonts successfully." << endl;
+            SizeDesc size;
+            size.pixelSize = 80;
+            size.space = 0.8;
+            size.gap = 0.1;
+            i18n2.setSize(&size);
+            const wchar_t *msg3 = L"粤BMW888";
+            cout << "Put 3";
+            int num = i18n2.putText(img_groud, msg3, Point(1280, 100), CV_RGB(0, 0, 0));
+            cout << num << " chars." << endl;
+        }
+
+        compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+        compression_params.push_back(80);
+        Mat new_img;
+        new_img.push_back(img);
+        new_img.push_back(img_groud);
+        imwrite("test.jpg", new_img, compression_params);
         fclose(fp);
     } catch (Exception e) {
         cout << e.what() << endl;
